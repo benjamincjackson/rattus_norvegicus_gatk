6fcd1950dbe71f368e65e837ec3918910bfa685b
benjamincjackson <benjamincjackson@gmail.com>
2017-08-15 12:02:05 +0100

    update scripts

--- 4_haplotype_caller/make_commands_file.sh
+++ 4_haplotype_caller/make_commands_file.sh
@@ -1,9 +1,9 @@
-grep "norvegicus" /localdisk/home/bjackso4/rattus_norvegicus_FASTQ/sample_file_table.txt | cut -f2 | sort | uniq | {
+grep "norvegicus\|rattus" /localdisk/home/bjackso4/rattus_norvegicus_FASTQ/sample_file_table.txt | cut -f2 | sort | uniq | {
 while read SAMPLE
 do
 	while read CONTIG
 	do
-		echo "nice -n 19 ./run_haplotype_caller.sh	/localdisk/home/bjackso4/rattus_norvegicus_GATK/3_merge_duplicates_out/${SAMPLE}.merged.sorted.dedupped.bam	${SAMPLE}	${CONTIG}" >> commands.txt
+		echo "nice -n 19 ./run_haplotype_caller.sh	/localdisk/home/bjackso4/rattus_norvegicus_GATK/3_mark_duplicates_out/${SAMPLE}.merged.sorted.dedupped.bam	${SAMPLE}	${CONTIG}" >> commands.txt
 	done</localdisk/home/bjackso4/rattus_norvegicus_reference/contigs_main.txt
 
 done
